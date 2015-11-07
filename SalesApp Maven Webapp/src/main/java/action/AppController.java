faee2249923a46a6fe37c2265c5aac4c3f345fd2
charlymayo <charlymayomail@gmail.com>
2015-11-06 21:50:45 -0600

    proyecto web
    proyecto que hizo jc en casa de chava

---
+++ SalesApp Maven Webapp/src/main/java/action/AppController.java
@@ -0,0 +1,25 @@
+package action;
+
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.ControllerAdviceBean;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AppController {
+
+}
