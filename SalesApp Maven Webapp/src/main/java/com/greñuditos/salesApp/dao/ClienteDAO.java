a0990f332203c3e74b54c958953fb0151fee830f
Irma Nataly Alonso de León <nataly.alonso@logistikapp.com>
2015-11-21 23:01:42 -0600

    clietdao

--- SalesApp Maven Webapp/src/main/java/com/greñuditos/salesApp/dao/ClienteDAO.java
+++ SalesApp Maven Webapp/src/main/java/com/greñuditos/salesApp/dao/ClienteDAO.java
@@ -1,9 +1,21 @@
 package com.greñuditos.salesApp.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.greñuditos.salesApp.dto.*;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public interface ClienteDAO {
-
+    int getCount();
+    ArrayList<Cliente> getClientes();
+    Cliente getClientById(int clientId);
+    void deleteClient(Cliente client);
+    void updateClient(Cliente client);
+    public Session openCurrentSessionwithTransaction();
+    public void closeCurrentSessionwithTransaction();
+    public void closeCurrentSession();
+    public Transaction getCurrentTransaction();
+    public Session getCurrentSession();
 }
