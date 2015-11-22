1be54154c459e607ecaea843774bc228992d8fc4
Irma Nataly Alonso de León <nataly.alonso@logistikapp.com>
2015-11-21 23:03:35 -0600

    service

--- SalesApp Maven Webapp/src/main/java/com/greñuditos/salesApp/service/impl/ServiceImpl.java
+++ SalesApp Maven Webapp/src/main/java/com/greñuditos/salesApp/service/impl/ServiceImpl.java
@@ -2,6 +2,7 @@ package com.greñuditos.salesApp.service.impl;
 
 
 import com.greñuditos.salesApp.dao.impl.*;
+import com.greñuditos.salesApp.dto.Cliente;
 import com.greñuditos.salesApp.dto.Producto;
 import com.greñuditos.salesApp.dto.Rol;
 import com.greñuditos.salesApp.service.Service;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 public class ServiceImpl implements Service {
 
     private ProductoDAOImpl productoDAOImpl;
+    private ClienteDAOImpl clienteDAOImpl;
     private RolDAOImpl rolDAOImpl;
 
     public void setProductoDAOImpl(ProductoDAOImpl productoDAOImpl){
@@ -21,7 +23,7 @@ public class ServiceImpl implements Service {
         this.rolDAOImpl = rolDAOImpl;
     }
 
-    public int getCount() {
+    public int getProductCount() {
         this.productoDAOImpl.openCurrentSessionwithTransaction();
         int result = this.productoDAOImpl.getCount();
         this.productoDAOImpl.closeCurrentSessionwithTransaction();
@@ -67,5 +69,43 @@ public class ServiceImpl implements Service {
         Rol rol = this.rolDAOImpl.getRolById(rolId);
         this.rolDAOImpl.closeCurrentSessionwithTransaction();
         return rol;
+    }
+
+    public int getClientCount() {
+        this.clienteDAOImpl.openCurrentSessionwithTransaction();
+        int result = this.clienteDAOImpl.getCount();
+        this.clienteDAOImpl.closeCurrentSessionwithTransaction();
+        return result;
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        this.clienteDAOImpl.openCurrentSessionwithTransaction();
+        ArrayList<Cliente> clientes = this.clienteDAOImpl.getClientes();
+        this.clienteDAOImpl.closeCurrentSessionwithTransaction();
+        return clientes;
+    }
+
+    public Cliente getClienteById(int clienteId) {
+        this.clienteDAOImpl.openCurrentSessionwithTransaction();
+        Cliente cliente = this.clienteDAOImpl.getClientById(clienteId);
+        this.clienteDAOImpl.closeCurrentSessionwithTransaction();
+        return cliente;
+    }
+
+    public void deleteCliente(int clienteId) {
+        this.clienteDAOImpl.openCurrentSessionwithTransaction();
+        Cliente cliente = this.clienteDAOImpl.getClientById(clienteId);
+        this.clienteDAOImpl.deleteClient(cliente);
+        this.clienteDAOImpl.closeCurrentSessionwithTransaction();
+    }
+
+    public void updateCliente(Cliente cliente) {
+        this.clienteDAOImpl.openCurrentSessionwithTransaction();
+        this.clienteDAOImpl.updateClient(cliente);
+        this.clienteDAOImpl.closeCurrentSessionwithTransaction();
+    }
+
+    public void setClienteDAOImpl(ClienteDAOImpl clienteDAOImpl) {
+        this.clienteDAOImpl = clienteDAOImpl;
     }
 }
