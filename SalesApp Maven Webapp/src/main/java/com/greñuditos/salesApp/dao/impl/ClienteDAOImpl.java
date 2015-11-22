536ee9d1cf9b60275077b7cf1c0e0719ccfcec8f
Irma Nataly Alonso de León <nataly.alonso@logistikapp.com>
2015-11-21 23:02:37 -0600

    clietdaoimpl

--- SalesApp Maven Webapp/src/main/java/com/greñuditos/salesApp/dao/impl/ClienteDAOImpl.java
+++ SalesApp Maven Webapp/src/main/java/com/greñuditos/salesApp/dao/impl/ClienteDAOImpl.java
@@ -1,12 +1,63 @@
 package com.greñuditos.salesApp.dao.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.greñuditos.salesApp.dao.*;
 import com.greñuditos.salesApp.dto.*;
-
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 
 public class ClienteDAOImpl implements ClienteDAO {
+    private SessionFactory sessionFactory;
 
+    public void setSessionFactory(SessionFactory sessionFactory){
+        this.sessionFactory = sessionFactory;
+    }
+
+    public Session openCurrentSessionwithTransaction() {
+        currentSession = sessionFactory.openSession();
+        currentTransaction = currentSession.beginTransaction();
+        return currentSession;
+    }
+
+    public void closeCurrentSessionwithTransaction() {
+        currentTransaction.commit();
+        currentSession.close();
+    }
+    public void closeCurrentSession(){
+        currentSession.close();
+    }
+
+    public Session getCurrentSession() {
+        return currentSession;
+    }
+
+    public Transaction getCurrentTransaction(){
+        return currentTransaction;
+    }
+
+    private Session currentSession;
+    private Transaction currentTransaction;
+    public int getCount() {
+        return ((Long)getCurrentSession().createQuery("select count(*) FROM Cliente").uniqueResult()).intValue();
+    }
+
+    public ArrayList<Cliente> getClientes() {
+        return new ArrayList<Cliente>(getCurrentSession().createQuery("FROM Cliente ").list());
+    }
+
+    public Cliente getClientById(int clientId) {
+        return (Cliente) getCurrentSession().get(Cliente.class, clientId);
+    }
+
+    public void deleteClient(Cliente client) {
+        getCurrentSession().delete(client);
+    }
+
+    public void updateClient(Cliente client) {
+        getCurrentSession().update(client);
+    }
 }
